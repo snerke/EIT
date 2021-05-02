a68feaaaf317834d53dab11942ce107a5430896f
sverrkju <sverrkju@stud.ntnu.no>
2021-05-02 17:42:11 +0200

    Updates

---
+++ precache-manifest.506db3de0a246ef7d10f93356a9351d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78e0d0450ad1f1de7ec5d66c5a4f53cb",
+    "url": "/EIT/index.html"
+  },
+  {
+    "revision": "89723efff539af773751",
+    "url": "/EIT/static/css/2.0a5b19c0.chunk.css"
+  },
+  {
+    "revision": "70423f811695a04b6e06",
+    "url": "/EIT/static/css/main.b822509f.chunk.css"
+  },
+  {
+    "revision": "89723efff539af773751",
+    "url": "/EIT/static/js/2.48059ed7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/EIT/static/js/2.48059ed7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70423f811695a04b6e06",
+    "url": "/EIT/static/js/main.a44e248c.chunk.js"
+  },
+  {
+    "revision": "9dfa9cd0d1529ae42232",
+    "url": "/EIT/static/js/runtime-main.63d6bff8.js"
+  }
+]);
